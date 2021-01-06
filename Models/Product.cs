924c1f6917ec4f05fa38e72c20d85c5de4423ff7
Angel <ngeljames2409@gmail.com>
2021-01-06 20:28:37 +0530

    final commit

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerWithEfSupport.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductName { get; set; }
+    }
+}
